b36c3a19c04669a65b6a3088b0d4e23d0955e02a
suhyun lee <skh3930@hanmail.net>
2021-05-20 11:40:58 +0900

    waiting number 로직 추가

--- order/src/main/java/coffee/OrderRepository.java
+++ order/src/main/java/coffee/OrderRepository.java
@@ -3,6 +3,5 @@ package coffee;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
-
-
+    public Long countByStatus(String status);
 }
