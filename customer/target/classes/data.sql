ef424ca37bd29b3f7815560c6a36c5eafa82a182
suhyun lee <skh3930@hanmail.net>
2021-05-20 14:05:22 +0900

    git target 하위 제거

--- customer/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into Customer_table (id, customer_point) values (1, 110000);
-insert into Customer_table (id, customer_point) values (2, 90000);
-insert into Customer_table (id, customer_point) values (3, 5000);
-insert into Customer_table (id, customer_point) values (4, 0);
