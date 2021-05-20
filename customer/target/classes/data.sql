4fa0d18e7c1cf822f426f8e86e2a19cf7395955f
suhyun lee <skh3930@hanmail.net>
2021-05-20 13:11:10 +0900

    order 수정

---
+++ customer/target/classes/data.sql
@@ -0,0 +1,4 @@
+insert into Customer_table (id, customer_point) values (1, 110000);
+insert into Customer_table (id, customer_point) values (2, 90000);
+insert into Customer_table (id, customer_point) values (3, 5000);
+insert into Customer_table (id, customer_point) values (4, 0);
