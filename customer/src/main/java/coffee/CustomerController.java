1660933cbae345da3311085761ef5e58d5c1deda
suhyun lee <skh3930@hanmail.net>
2021-05-20 13:20:40 +0900

    불필요라이브러리 제거

--- customer/src/main/java/coffee/CustomerController.java
+++ customer/src/main/java/coffee/CustomerController.java
@@ -1,32 +1,26 @@
 package coffee;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import java.util.Optional;
 
- @RestController
- public class CustomerController {
+@RestController
+public class CustomerController {
 
-        @Autowired 
+        @Autowired
         CustomerRepository customerRepository;
 
-@RequestMapping(value = "/customers/checkAndModifyPoint",
-        method = RequestMethod.GET,
-        produces = "application/json;charset=UTF-8")
+        @RequestMapping(value = "/customers/checkAndModifyPoint", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
 
-// public boolean checkAndModifyPoint(HttpServletRequest request, HttpServletResponse response)
-public boolean checkAndModifyPoint(@RequestParam("customerId") Long customerId, 
-                                   @RequestParam("price")  Integer price)
-        throws Exception {
-        System.out.println("##### /customer/checkAndModifyPoint  called #####");
-//Edited Source
+        // public boolean checkAndModifyPoint(HttpServletRequest request,
+        // HttpServletResponse response)
+        public boolean checkAndModifyPoint(@RequestParam("customerId") Long customerId,
+                        @RequestParam("price") Integer price) throws Exception {
+                System.out.println("##### /customer/checkAndModifyPoint  called #####");
+                // Edited Source
                 boolean result = false;
 
                 Optional<Customer> customerOptional = customerRepository.findById(customerId);
@@ -35,10 +29,10 @@ public boolean checkAndModifyPoint(@RequestParam("customerId") Long customerId,
                         result = true;
                         customer.setCustomerPoint(customer.getCustomerPoint() - price);
                         customerRepository.save(customer);
-                } 
+                }
 
-           return result;
+                return result;
 
         }
 
- }
+}
