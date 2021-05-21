ef63ea6fdf53ad8cf7ee0dde194d71b07584e4dc
suhyun lee <skh3930@hanmail.net>
2021-05-21 11:00:56 +0900

    서킷브레이커 옵션 변경

--- order/src/main/java/coffee/OrderApplication.java
+++ order/src/main/java/coffee/OrderApplication.java
@@ -1,17 +1,20 @@
 package coffee;
+
 import coffee.config.kafka.KafkaProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderApplication {
     protected static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         applicationContext = SpringApplication.run(OrderApplication.class, args);
     }
